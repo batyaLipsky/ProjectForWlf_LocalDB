790b1538606ce6f5fc17bfe1d1aa37c1c6c305eb
bl <milgaplus@gmail.com>
2020-06-09 17:53:47 +0300

    api project with correct EF

---
+++ APITravel_In_Group/API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UserController : ApiController
+    {
+        [HttpGet]  
+        [Route("Users")]
+
+        public IHttpActionResult GetUsers()
+        {
+
+            return Ok(BL.UserBL.GetUsers());
+           // return Ok( "dd");
+        }
+
+
+        //that method is also correct, even though it doesn't have [Route("Users")]
+        [HttpGet]
+        public IHttpActionResult Login()
+        {
+            return Ok("correct method");
+        }
+    }
+}
---
+++ APITravel_In_Group/BL/UserBL.cs
@@ -0,0 +1,32 @@
+﻿using DAL.EF;
+//using DAL.EntitiesFramwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public static class UserBL
+    {
+        public static object GetUsers()
+        {
+            try
+            {
+
+                using (Travel_In_GroupDBEntities1 db = new Travel_In_GroupDBEntities1())
+                {
+                
+                    return db.Users.ToList();
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("error");
+            }
+        }
+    }
+
+}
